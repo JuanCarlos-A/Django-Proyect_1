7d0bc735fee989501dfa1b333457939b57fbf533
JuanCarlos-A <juancar2805@gmail.com>
2024-05-16 17:17:58 -0500

    Templates o Plantillas en Django

--- productly/productos/views.py
+++ productly/productos/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Producto
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola Mundo!')
+    productos = Producto.objects.all()
+    return render(
+        request,
+        'index.html',
+        context={'productos' : productos }
+        )
+    
