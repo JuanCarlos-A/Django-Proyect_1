5d3cbbf15aa776a1a10385b0e0cdb5efcb4ebc5d
JuanCarlos-A <juancar2805@gmail.com>
2024-05-22 09:50:20 -0500

    Nueva organizacion en el modulo de url dentro de la aplicacion de productos

--- productly/productos/urls.py
+++ productly/productos/urls.py
@@ -4,8 +4,18 @@ from . import views
 app_name = 'productos'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('formulario', views.formulario, name='formulario'),
+    path(
+        '',
+        views.index, 
+        name='index'
+        ),
+        
+    path(
+        'formulario', 
+        views.formulario, 
+        name='formulario'
+        ),
+
     path(
         '<int:producto_id>',
         views.detalle, 
